87c6f02b1e4e2b228a3bba58b934b780a9b6edb4
taio-ops <tayopopoola92@gmail.com>
2025-11-04 11:43:14 +0100

    Used queryParams to get returned data that are assigned to variables when payment_callback route is called after payment status is defined.

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ const App = () => {
           <Route path="login" element={<LoginPage/>} />
           <Route path="profile" element={<UserProfilePage/>} />
           <Route path="*" element={<NotFoundPage />} />
-          <Route path="payment-status" element={<PaymentStatusPage />} />
+          <Route path="payment-status" element={<PaymentStatusPage setNumberCartItems={setNumberCartItems} />} />
 
           </Route>
         </Routes>
--- src/components/payments/PaymentStatusPage.jsx
+++ src/components/payments/PaymentStatusPage.jsx
@@ -1,16 +1,43 @@
-import { Link } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { Link, useLocation } from "react-router-dom"
+import api from "../../api";
 
 
-function PaymentStatusPage() {
+function PaymentStatusPage({setNumberCartItems}) {
+
+  const [statusMessage, setStatusMessage] = useState('Verifying your payment');
+  const [statusSubMessage, setStatusSubMessage] = useState('Wait a moment, your payment is being verified!');
+  const location = useLocation();
+
+  useEffect(function(){
+
+    const queryParams = new URLSearchParams(location.search);
+    const status = queryParams.get('status')
+    const txRef = queryParams.get('tx_ref')
+    const transactionId = queryParams.get('transaction_id')
+
+    if(status && txRef && transactionId){
+      api.post(`payment_callback/?status=${status}&tx_ref=${txRef}&transaction_id=${transactionId}`)
+      .then(res => {
+          setStatusMessage(res.data.message)
+          setStatusSubMessage(res.data.subMessage)
+          localStorage.removeItem('cart_code')
+          setNumberCartItems(0)
+      })
+
+      .catch(err => console.log(err.message)) 
+    }
+  },[])
+
   return (
     <header className='py-5' style={{ backgroundColor: '#6050DC' }}>
         <div className='container px-4 px-lg-5 my-5'>
             <div className='text-center text-white'>
-                <h2 className='display-4 fw-bold'>Verifying Payment!</h2>
-                <p className='lead fw-normal text-white-75 mb-4'>Give us a moment we are verifying your payment!.</p>
+                <h2 className='display-4 fw-bold'>{statusMessage}</h2>
+                <p className='lead fw-normal text-white-75 mb-4'>{statusSubMessage}</p>
                 <span>
-                    <Link to='/#shop' className='btn btn-light btn-lg px-4 py-2 mx-3'>View Order Details</Link>
-                    <Link to='/#shop' className='btn btn-light btn-lg px-4 py-2 mx-3'>Continue Shopping</Link>
+                    <Link to='/profile' className='btn btn-light btn-lg px-4 py-2 mx-3'>View Order Details</Link>
+                    <Link to='/' className='btn btn-light btn-lg px-4 py-2 mx-3'>Continue Shopping</Link>
                 </span>
             </div>
         </div>
