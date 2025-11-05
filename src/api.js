2dfaca230438cab32e002ee224ec9e7aae711c68
taio-ops <tayopopoola92@gmail.com>
2025-11-05 15:59:38 +0100

    build

--- src/api.js
+++ src/api.js
@@ -1,10 +1,12 @@
 import axios from 'axios'
 import {jwtDecode} from 'jwt-decode'
 
-export const BASE_URL =  "http://127.0.0.1:8000"
+// export const BASE_URL =  "http://127.0.0.1:8000"
+
+export const BASE_URL = import.meta.env.VITE_BASE_URL || "http://127.0.0.1:8000";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000"
+    baseURL: BASE_URL
 })
 
 api.interceptors.request.use(
